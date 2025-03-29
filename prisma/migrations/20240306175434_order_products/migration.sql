e6e9ff71d8aac99672f9faa27abde2b4fb5dcaca
aTpia12 <99981266+aTpia12@users.noreply.github.com>
2025-03-29 08:01:40 -0600

    Update migration.sql

--- prisma/migrations/20240306175434_order_products/migration.sql
+++ prisma/migrations/20240306175434_order_products/migration.sql
@@ -1,30 +1,27 @@
 -- CreateTable
-CREATE TABLE `Order` (
-                         `id` INT NOT NULL AUTO_INCREMENT,
-                         `name` TEXT NOT NULL,
-                         `total` DOUBLE NOT NULL,
-                         `date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                         `status` BOOLEAN NOT NULL DEFAULT FALSE,
-                         `orderReadyAt` TIMESTAMP(3),
+CREATE TABLE "Order" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "total" DOUBLE PRECISION NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" BOOLEAN NOT NULL DEFAULT false,
+    "orderReadyAt" TIMESTAMP(3),
 
-                         PRIMARY KEY (`id`)
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
 );
 
--- Create Table
-CREATE TABLE `OrderProducts` (
-                                 `id` INT NOT NULL AUTO_INCREMENT,
-                                 `orderId` INT NOT NULL,
-                                 `productId` INT NOT NULL,
+-- CreateTable
+CREATE TABLE "OrderProducts" (
+    "id" SERIAL NOT NULL,
+    "orderId" INTEGER NOT NULL,
+    "productId" INTEGER NOT NULL,
 
-                                 PRIMARY KEY (`id`)
+    CONSTRAINT "OrderProducts_pkey" PRIMARY KEY ("id")
 );
 
--- Add Foreign Key
-ALTER TABLE `OrderProducts`
-    ADD CONSTRAINT `OrderProducts_orderId_fkey`
-        FOREIGN KEY (`orderId`) REFERENCES `Order`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+-- AddForeignKey
+ALTER TABLE "OrderProducts" ADD CONSTRAINT "OrderProducts_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
--- Add Foreign Key
-ALTER TABLE `OrderProducts`
-    ADD CONSTRAINT `OrderProducts_productId_fkey`
-        FOREIGN KEY (`productId`) REFERENCES `Product`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+-- AddForeignKey
+ALTER TABLE "OrderProducts" ADD CONSTRAINT "OrderProducts_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
