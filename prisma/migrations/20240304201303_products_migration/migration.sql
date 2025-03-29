a316adb5c4977fc455280570a2dc787f14947b41
aTpia12 <99981266+aTpia12@users.noreply.github.com>
2025-03-29 08:01:16 -0600

    Update migration.sql

--- prisma/migrations/20240304201303_products_migration/migration.sql
+++ prisma/migrations/20240304201303_products_migration/migration.sql
@@ -1,15 +1,14 @@
 -- CreateTable
-CREATE TABLE `Product` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `name` TEXT NOT NULL,
-    `price` DOUBLE NOT NULL,
-    `image` TEXT NOT NULL,
-    `categoryId` INT NOT NULL,
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "image" TEXT NOT NULL,
+    "categoryId" INTEGER NOT NULL,
 
-    PRIMARY KEY (`id`)
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
 );
 
--- Add Foreign Key
-ALTER TABLE `Product`
-    ADD CONSTRAINT `Product_categoryId_fkey`
-        FOREIGN KEY (`categoryId`) REFERENCES `Category`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
